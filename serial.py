17bf4cbdc24847fc2ceff20b7f1c919c8225fdb7
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-28 15:24:50 -0800

    updated doc strings

--- serial.py
+++ serial.py
@@ -20,14 +20,15 @@ class SerialGenerator:
     100
     """
 
-    def __init__(self, start):
-        """initialize current number as 1 before start input"""
+    def __init__(self, start=100):
+        """initialize current number as None and record start"""
 
         self.start = start
         self.current_number = None
 
     def generate(self):
-        """add number to current number and return"""
+        """add number to current number if not None and return
+        for current number None (initial value) set to start and return"""
 
         if self.current_number is None:
             self.current_number = self.start
@@ -37,7 +38,7 @@ class SerialGenerator:
         return self.current_number
 
     def reset(self):
-        """return current number to starting position"""
+        """reset current number to None to restart at start """
 
         self.current_number = None
 
