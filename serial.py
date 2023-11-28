cb776505156ebc054f5d56c03d1e5f6db47ae66a
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-28 15:22:56 -0800

    pre code review

--- serial.py
+++ serial.py
@@ -1,6 +1,8 @@
+""" serial number generator """
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +19,25 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """initialize current number as 1 before start input"""
+
+        self.start = start
+        self.current_number = None
+
+    def generate(self):
+        """add number to current number and return"""
+
+        if self.current_number is None:
+            self.current_number = self.start
+        else:
+            self.current_number += 1
+
+        return self.current_number
+
+    def reset(self):
+        """return current number to starting position"""
+
+        self.current_number = None
+
